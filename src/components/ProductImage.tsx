fe97e06fad816eb9d93aab78dda71dae4fd16da4
NicoHC185 <nikohidalgo.castillo@gmail.com>
2025-03-07 20:41:57 -0300

    :bug: fixed case no img

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { CSSProperties, useContext, useMemo } from "react"
 import { ProductContext } from "./ProductCard"
-import noImage from '../assets/no-image.jpg'
 import styles from '../styles/styles.module.css'
 
 export interface Props {
@@ -15,15 +14,15 @@ export const ProductImage = ({ img, className, style }: Props) => {
     const { product } = useContext(ProductContext)
 
     const imgSrc = useMemo(() => {
-        return product?.img ?? img
+        return product?.img || img || '../assets/no-image.jpg'
     }, [product, img])
 
     return (
         <img
             className={`${styles.productImg} ${className}`}
             style={style}
-            src={imgSrc || noImage}
-            alt={imgSrc ? 'Product img' : 'no image'}
+            src={imgSrc}
+            alt={'Product img'}
         />
     )
 }
